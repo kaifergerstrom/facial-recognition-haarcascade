be0fe36749c1ac56257244bc7183e808961debf5
Kai Fergerstrom <kaifergerstrom@gmail.com>
2020-04-02 17:06:14 -0400

    Working recognizer and face loading script

--- drawer.py
+++ drawer.py
@@ -5,76 +5,49 @@ import pickle
 
 face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 
-PICKLE_FILE = "pickles/known_faces.pckl"  # File path for pickles of known faces
-KNOWN_FACES_DIR = "known_faces"  # Path for testing images
+known_faces = pickle.loads(open("pickles/embeddings.pickle", "rb").read())
+known_names = pickle.loads(open("pickles/names.pickle", "rb").read())
+
 TOLERANCE = 0.6  # Tolerance for face detection
 
-def save_pickle(var, file_path):
-	"""
-	Input variable and export as serialized pickle to file_path
-	"""
-	f = open(file_path, 'wb')
-	pickle.dump(var, f)
-	f.close()
+def indentify_face(image):
+	print("Scanning face...")
+	global known_faces, known_names
+	
+	locations = face_recognition.face_locations(image, model="cnn")
+	encodings = face_recognition.face_encodings(image, locations)
+	face_img = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-def load_pickle(file_path):
-	"""
-	load up pickle file from path return the variable
-	"""
-	f = open(file_path, 'rb')
-	obj = pickle.load(f)
-	f.close()
-	return obj
+	for face_encoding, face_location in zip(encodings, locations):
+		results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
+		Match = None
+		if True in results:
+			match = known_names[results.index(True)]
+			print("Match founds: {}".format(match))
 
-def load_training_faces():
-	"""
-	Create the training dictionary of faces
-	"""
-	print("Loading training faces...")
-
-	known_faces = {}
-
-	for subdir, dirs, files in os.walk(KNOWN_FACES_DIR):
-
-		label = os.path.basename(subdir)  # Get name of folder
-		known_faces[label] = []
-
-		for file in files:  # Loop through each file in the folder
-			
-			image_dir = os.path.join(subdir, file)  # Create full path for each file
-
-			# Load and create encoding for each image
-			image = face_recognition.load_image_file(image_dir)
-			encoding = face_recognition.face_encodings(image)[0]
-			
-			# Create reference dictionary for known faces
-			known_faces[label].append(encoding)
-
-	return known_faces
-
-def get_known_faces():
-	"""
-	Get the known faces variable, if does not exist create it, but if it does just fetch pickle variable
-	"""
-	if not os.path.exists(PICKLE_FILE):
-		known_faces = load_training_faces()
-		save_pickle(known_faces, PICKLE_FILE)
-	else:
-		known_faces = load_pickle(PICKLE_FILE)
-
-
-# Main openCV loop
 
 cam = cv2.VideoCapture(0)  # Video capture instance
 
 while True:
 	
-	_, img = cam.read()
+	_, frame = cam.read()
+	display = frame.copy()  # Display frame with drawing
 
-	cv2.imshow('my webcam', img)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	if cv2.waitKey(1) == 27: 
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+	for face_id, (x, y, w, h) in enumerate(faces):
+		cv2.rectangle(display, (x,y), (x+w, y+h), (255, 0, 0), 2)
+		
+		image = frame.copy()[y:y+h, x:x+w]	
+				
+	cv2.imshow('webcam', display)
+	
+	key = cv2.waitKey(1)
+	if  key == ord("q"): 
 		break  # esc to quit
+	elif key == ord("c"):
+		indentify_face(image)
 
 cv2.destroyAllWindows()
-
