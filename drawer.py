78465193eccf48909cc1d12f6c04ed69b76240ef
Kai Fergerstrom <kaifergerstrom@gmail.com>
2020-04-02 12:55:21 -0400

    Loaded haarcascades, C:/Users/kaife/AppData/Local/Programs/Python/Python38/python.exe c:/Users/kaife/OneDrive/Desktop/Projects/facial-recognition-haarcascade/drawer.py

--- drawer.py
+++ drawer.py
@@ -3,19 +3,33 @@ import face_recognition
 import os
 import pickle
 
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+
+PICKLE_FILE = "pickles/known_faces.pckl"  # File path for pickles of known faces
+KNOWN_FACES_DIR = "known_faces"  # Path for testing images
+TOLERANCE = 0.6  # Tolerance for face detection
+
 def save_pickle(var, file_path):
+	"""
+	Input variable and export as serialized pickle to file_path
+	"""
 	f = open(file_path, 'wb')
 	pickle.dump(var, f)
 	f.close()
 
 def load_pickle(file_path):
+	"""
+	load up pickle file from path return the variable
+	"""
 	f = open(file_path, 'rb')
 	obj = pickle.load(f)
 	f.close()
 	return obj
 
 def load_training_faces():
-
+	"""
+	Create the training dictionary of faces
+	"""
 	print("Loading training faces...")
 
 	known_faces = {}
@@ -38,19 +52,20 @@ def load_training_faces():
 
 	return known_faces
 
+def get_known_faces():
+	"""
+	Get the known faces variable, if does not exist create it, but if it does just fetch pickle variable
+	"""
+	if not os.path.exists(PICKLE_FILE):
+		known_faces = load_training_faces()
+		save_pickle(known_faces, PICKLE_FILE)
+	else:
+		known_faces = load_pickle(PICKLE_FILE)
 
-PICKLE_FILE = "pickles/known_faces.pckl"
-KNOWN_FACES_DIR = "known_faces"
-TOLERANCE = 0.6
 
+# Main openCV loop
 
-if not os.path.exists(PICKLE_FILE):
-	known_faces = load_training_faces()
-	save_pickle(known_faces, PICKLE_FILE)
-else:
-	known_faces = load_pickle(PICKLE_FILE)
-
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(0)  # Video capture instance
 
 while True:
 	
